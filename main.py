f695d7e05c354fa7accb41b3d8396d017f9d328a
Fornicis <blizzard_91@hotmail.co.uk>
2024-08-17 09:00:19 +0100

    Added solve functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import Window, Line, Point
 from maze import Maze
+import sys
 
 def main():
     num_rows = 12  # Number of rows in the maze
@@ -11,12 +12,17 @@ def main():
     # Calculate the size of each cell based on the screen size and margins
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
-
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)  # Create a window with the specified dimensions
 
     # Create a maze with the calculated parameters and draw it in the window
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 5074645)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 69420)
+    print("Maze Created")
+    is_solveable = maze.solve()
+    if not is_solveable:
+        print("Maze cannot be solved!")
+    else:
+        print("Maze solved!")
 
     # Keep the window open until manually closed
     win.wait_for_close()
